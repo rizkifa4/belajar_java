58a08f18599584bb044a4d99160b6e746b3f6399
rizkifa4 <rizkifachrozy@gmail.com>
2023-01-28 09:53:55 +0700

    pindah folder

---
+++ 06-Studi-Kasus/SuhuAir/src/com/javafundamental/suhuair/SuhuAir.java
@@ -0,0 +1,23 @@
+package com.javafundamental.suhuair;
+
+import java.util.Scanner;
+
+public class SuhuAir {
+    public static void main(String[] args) {
+        int T;
+
+        System.out.println("Contoh IF tiga kasus");
+        System.out.print("Suhu (der. C) = ");
+
+        Scanner scanner = new Scanner(System.in);
+        T = scanner.nextInt();
+
+        if (T < 0){
+            System.out.println("Wujud air beku " + T);
+        } else if ((0 <= T) && (T <= 100)) {
+            System.out.println("Wujud air cair " + T);
+        } else if (T>100) {
+            System.out.println("Wujid air uap/gas " + T);
+        }
+    }
+}
